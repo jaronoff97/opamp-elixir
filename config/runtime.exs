9cd36d08fb652165be61eeba3b2ac7c713e33a33
Jacob Aronoff <jacobaronoff45@gmail.com>
2023-10-23 12:51:36 -0400

    secret

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :opamp_server, OpAMPServer.Repo,
-    # ssl: true,
+    ssl: false,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
@@ -61,7 +61,8 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: port,
+      transport_options: [socket_opts: maybe_ipv6]
     ],
     secret_key_base: secret_key_base
 
---
+++ rel/overlays/bin/server.bat
@@ -0,0 +1,2 @@
+set PHX_SERVER=true
+call "%~dp0\opamp_server" start
